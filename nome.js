055862a75bf213184238f2cf51a12d419f228db4
gabrielapaulino18 <gabrielappsilva18@gmail.com>
2022-03-03 22:24:40 -0300

    testando logica em javascript

---
+++ nome.js
@@ -0,0 +1,9 @@
+//exibir meu nome
+//console.log('gabriela);
+var nome = 'gabriela';
+var sobrenome = 'Silva Paulino';
+var nomecompleto;
+var idade = 16;
+nomecompleto = nome + sobrenome;
+console.log(nomecompleto);
+console.log( "Meu nome é:"+" "+nome + " "+sobrenome );
